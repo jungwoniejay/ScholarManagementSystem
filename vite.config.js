458d0228d810722b14e17828e7bfcbbd98e9c1c4
Jing17Git <richarddbautista1@gmail.com>
2026-02-12 14:30:17 +0800

    Update Dockerfile and Vite config for production

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: mode === 'development',
+            refresh: true,
         }),
     ],
     server: {
-        host: 'localhost',    // <-- change this
-        port: 5174,           // <-- fixed port
-        strictPort: true,     // ensures it doesn't pick random port
-        cors: true,
+        host: '0.0.0.0',
+        port: 5174,
+        strictPort: true,
     },
-    build: {
-        manifest: true,           // Laravel needs this
-        outDir: 'public/build',   // must be public/build
-        emptyOutDir: true,
-        assetsDir: 'assets',
-    },
-    base: mode === 'production' ? '/build/' : '/',
-}));
+});
